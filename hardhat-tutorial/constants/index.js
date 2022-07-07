802c8022738ca63929bf01065f91d1ed91c132fa
krystal lee <theekrystallee@gmail.com>
2022-07-06 19:28:00 -0700

    ready for prod

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x66aADd07D35B1433754eb509978c0a3DC7D5fbD6";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
